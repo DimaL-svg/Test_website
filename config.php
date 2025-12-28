f3acca3151234ac763a7d23b26a0561b0b02ce57
Dmytro <dimalitvinenko@gmail.com>
2025-12-28 18:37:19 +0200

    Auto stash before rebase of "feature/welcome"

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $pass = '';
 try 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+ catch (PDOException $e) {
     die("Помилка підключення: " . $e->getMessage());
 }
-?>
+?
