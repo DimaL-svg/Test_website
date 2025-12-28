97ed4733152ae00d5aab7afc28bed070a363a805
Dmytro <dimalitvinenko@gmail.com>
2025-12-28 18:42:13 +0200

    Refactor(config): Rename PDO variable and remove closing PHP tag

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ $user = 'root';
 $pass = ''; 
 
 try 
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Помилка підключення: " . $e->getMessage());
 }
-?>
+?
