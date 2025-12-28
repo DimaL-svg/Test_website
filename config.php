e4da1daf9be14bc359e64cf2cacca880c3a51a5b
Dmytro <dimalitvinenko@gmail.com>
2025-12-28 18:33:01 +0200

    Chore: Remove opening brace from PDO connection try block

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $dbname = 'lab14_db';
 $user = 'root';
 $pass = ''; 
 
-try {
+try 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
