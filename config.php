9dd456ad49d0f4d18fdf2599db592b07ad5dd2fc
Dmytro <dimalitvinenko@gmail.com>
2025-12-28 21:23:36 +0200

    Adjust end of file structure in `config.php`

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ $dbname = 'lab14_db';
 $user = 'root';
 $pass = ''; 
 
-try { 
+try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Помилка підключення: " . $e->getMessage());
-}
-?>
+
+?
