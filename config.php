f80b9d7109b3c1dddf0bba0490b248f7245f25d7
Dmytro <dimalitvinenko@gmail.com>
2025-12-28 18:28:24 +0200

    Adds database connection configuration
    Establishes a connection to the MySQL database using PDO.
    This configuration is necessary for subsequent database operations
    within the application.
    
    The configuration includes the host, database name, username,
    and password.
    Also, it sets the PDO error mode to exception for better error handling.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'lab14_db';  
+$user = 'root';
+$pass = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Помилка підключення: " . $e->getMessage());
+}
+?>
